44002e626c83651fcb33347d0411ecd9a0a87bb4
Yoseph Santos <yoseph.santos@bematech.com.br>
2017-11-20 17:19:51 -0200

    list of links and readme

--- public/js/index.js
+++ public/js/index.js
@@ -3,48 +3,61 @@ const uuidv1 = require('uuid/v1');
 const Crawler = require('crawler');
 
 // Crawler config
-const crawler = new Crawler();
 const baseUrl = 'https://en.wikipedia.org';
 const mainLink = '/wiki/Europe';
 const urlMain = baseUrl + mainLink;
+const eachElement = '.mw-parser-output p';
+const anchorElement = 'a[href^="/wiki/"]';
 
 // IndexJs config
-let linkListStoreOfficial = [];
+let linkListStoreMain = [];
 let level = 0;
 let limitRequestsByLevel = 100;
 
 // Database config
-var firebase = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
+var firebase = require('firebase-admin');
+var serviceAccount = require('./serviceAccountKey.json');
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://wikipedia-link.firebaseio.com"
+  databaseURL: 'https://wikipedia-link.firebaseio.com'
 });
 
+/*
+    Request for all links of parameter
+*/
 function doRequestsList(list) {
     if(list.length > 0) {
         list.forEach(function(link) {
             if(link && link.url) {
-                crawling.queue(link.url);   
+                crawler.queue(link.url);   
             }
         });
     }
 }
 
+/*
+    Verify if object has parentId
+*/
 function hasParentIdToSave() {
-    return linkListStoreOfficial[level-1] 
-    && linkListStoreOfficial[level-1][level-1] 
-    && linkListStoreOfficial[level-1][level-1].parentId;
+    return linkListStoreMain[level-1] 
+    && linkListStoreMain[level-1][level-1] 
+    && linkListStoreMain[level-1][level-1].parentId;
 }
 
+/*
+    Store the link list by parentId in Array linkListStoreMain.
+*/ 
 function readLinkNext(linkListStore) {
     if(linkListStore.length > 0) {
-        linkListStoreOfficial.push(linkListStore);
+        linkListStoreMain.push(linkListStore);
         
-        if(hasParentIdToSave()) doRequestsList(linkListStoreOfficial[level-1]);
+        if(hasParentIdToSave()) doRequestsList(linkListStoreMain[level-1]);
     }
 }
 
+/*
+    Store data in firebase.
+*/
 function saveLink(parentIdLink, url, id) {
     firebase.database().ref('links/' + parentIdLink).push({
         url: url,
@@ -52,13 +65,18 @@ function saveLink(parentIdLink, url, id) {
     });
 }
 
+/*
+    Generate parentId.
+    Add another level.
+    Get links of the page.
+*/ 
 function readLink(response, linkListStore) {
     let parentId = uuidv1();
     
     level++;
     
-    response.$('.mw-parser-output p').each(function() {
-        let link = response.$(this).find("a[href^='/wiki/']").attr('href');
+    response.$(eachElement).each(function() {
+        let link = response.$(this).find(anchorElement).attr('href');
         
         if(link) {
             const linkToParser = baseUrl + link;
@@ -71,7 +89,13 @@ function readLink(response, linkListStore) {
     
 }
 
-let crawling = new Crawler({
+
+/*
+    Do the request for crawl a page.
+    Get all links of the page and 
+    do new requests to the children of this links. 
+*/
+let crawler = new Crawler({
     maxConnections : 10,
     callback : function (error, response, done) {
         let linkListStore = new Array();
@@ -89,8 +113,11 @@ let crawling = new Crawler({
     }
 });
 
+/*
+    Execute the crawler with the main page.
+*/
 function main(urlMain) {   
-    crawling.queue(urlMain);
+    crawler.queue(urlMain);
 }
 
 main(urlMain);
